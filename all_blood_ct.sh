1e6f4f8524539665973c22168a4713a82bee64c2
mding010705 <mding@rstudio01.camhres.ca>
2025-04-06 11:59:27 -0400

    organization

--- all_blood_ct.sh
+++
@@ -1,7 +0,0 @@
-count=0
-for filename in ~/sc-tprs-mdd/immune_cell_models/*.db; do
-  fn=`basename $filename .db`
-  echo $fn
-  count=$((count+1))
-done
-echo $count
--- misc_bash.sh
+++
@@ -1,2 +0,0 @@
-awk '/^[0-9]+/ {print $2}' /external/rprshnas01/kcni/mding/sc-tprs-mdd/ribo_overlap.tsv >\
-/external/rprshnas01/kcni/mding/sc-tprs-mdd/ribo_genes.txt
